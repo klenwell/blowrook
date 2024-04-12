3859ace93ac2ebafeb0f406ab3cbd0667ef3f399
klenwell <klenwell@gmail.com>
2024-04-12 16:46:47 -0700

    Makes a draggable rook using d3.

--- experiments/002/onload.js
+++ experiments/002/onload.js
@@ -4,3 +4,24 @@ document.addEventListener("DOMContentLoaded", function() {
     const titleTag = document.getElementsByTagName('title')[0];
     h2Title.innerText = titleTag.innerText;
 })
+
+class Court {
+    constructor(selector, width, height) {
+        this.selector = selector
+        this.width = width;
+        this.height = height;
+        this.svg = this.initSvg();
+    }
+
+    initSvg() {
+        return d3.select(this.selector).append("svg")
+            .attr("width", this.width)
+            .attr("height", this.height)
+            .attr('fill', 'white');
+    }
+
+    addRook(rook) {
+        this.rook = rook;
+        this.rook.initSvg(this.svg);
+    }
+}
