4f6d74fccdfa7589c19d8071c7de1032fa26ded0
klenwell <klenwell@gmail.com>
2024-05-05 12:22:39 -0700

    Stubs out EXP 8 app.

---
+++ experiments/008/blowrook.js
@@ -0,0 +1,12 @@
+const AppSettings = {
+};
+
+class BlowrookApp {
+    constructor() {
+        console.log('BlowrookApp#constructor')
+    }
+
+    run() {
+        console.log('BlowrookApp#run')
+    }
+}
