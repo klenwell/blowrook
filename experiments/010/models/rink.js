3c65fe13f12ce67964f02daf86ec2e88a61f67c2
klenwell <klenwell@gmail.com>
2024-06-02 21:14:10 -0700

    Stubs out rink model.

---
+++ experiments/010/models/rink.js
@@ -0,0 +1,17 @@
+const RinkDimensions = {
+    X: 100,
+    Y: 100,
+    R: 100
+}
+
+class Rink {
+    constructor() {
+        this.radius = RinkDimensions.R;
+        this.x = RinkDimensions.X;
+        this.y = RinkDimensions.Y;
+    }
+
+    containsRook(rook) {
+        throw 'TODO'
+    }
+}
