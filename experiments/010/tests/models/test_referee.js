a3c6f4d64d79abaf028687be170cacb72783abf2
klenwell <klenwell@gmail.com>
2024-06-08 14:55:09 -0700

    Adds tests for isValidMove method.

--- experiments/010/tests/models/test_referee.js
+++ experiments/010/tests/models/test_referee.js
@@ -1,4 +1,10 @@
 module('Referee', hooks => {
+    const playerParams = {
+        id: 1,
+        name: 'alpha',
+        email: 'alpha@example.com'
+    };
+
     module('.constructor', hooks => {
         test('when passed a match with rink', assert => {
             let params = { rink: new Rink() };
@@ -19,6 +25,52 @@ module('Referee', hooks => {
 
         test('when not passed a match', assert => {
             assert.throws(() => new Referee(), /TypeError/);
+        });
+    });
+
+    module('#isValidMove', hooks => {
+        hooks.beforeEach(() => {
+            let params = { rink: new Rink() };
+            let match = new Match(params);
+            this.referee = new Referee(match);
+
+            this.player = new Player(playerParams);
+        });
+
+        hooks.afterEach(() => {
+            this.referee = undefined;
+            this.player = undefined;
+        });
+
+        test('when passed a valid move', assert => {
+            let rookParams = this.referee.rink.circle
+            let rook = new Rook(rookParams);
+
+            let params = {
+                player: this.player,
+                rook: rook
+            };
+            let move = new Move(params);
+
+            assert.ok(this.referee.isValidMove(move));
+        });
+
+        test('when passed an invalid move', assert => {
+            let rookParams = this.referee.rink.circle
+            rookParams.r += 1;
+            let rook = new Rook(rookParams);
+
+            let params = {
+                player: this.player,
+                rook: rook
+            };
+            let move = new Move(params);
+
+            assert.notOk(this.referee.isValidMove(move));
+        });
+
+        test('when passed no move', assert => {
+            assert.throws(() => this.referee.isValidMove(), /TypeError/);
         });
     });
 });
